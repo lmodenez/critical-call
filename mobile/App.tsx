5b4948a7b8f630f30d6ac27826a9efb4cc4ba87a
Luiz Pedro Modenez <lmodenez98@gmail.com>
2021-06-17 18:08:59 -0300

    first commit: mobile app

---
+++ mobile/App.tsx
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Home />
+    </NavigationContainer>
+  );
+}
