4be86815129ad614b34bb9d5e5ec353e1142d9cf
Luiz Pedro Modenez <lmodenez98@gmail.com>
2021-06-22 21:59:38 -0300

    feat: get data from backend in frontend

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,8 +1,38 @@
+import { useCallback, useEffect, useState } from 'react';
+import api from '../services/api';
+
 import styles from './app.module.scss';
 
 import { AiOutlineMenu } from 'react-icons/ai';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Loader from 'react-loader-spinner';
+
+type ticketType = {
+  id: number;
+  solicitante: string;
+  mecanismo: string;
+  categoria: string;
+  subcategoria: string;
+  data_abertura: Date;
+};
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tickets, setTickets] = useState<ticketType[]>([]);
+
+  useEffect(() => {
+    api.get('/tickets').then((response) => {
+      setTickets(response.data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  async function handleDelete(id: number) {
+    api.post(`/tickets/${id}`).then((response) => {
+      console.log(response);
+    });
+  }
+
   return (
     <main className={styles.container}>
       <div className={styles.wrapper}>
@@ -18,53 +48,49 @@ export default function App() {
               <th>Ações</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Suporte</td>
-              <td>Silvia</td>
-              <td>Coletor</td>
-              <td>Performance</td>
-              <td>21/06/2021</td>
-              <td>
-                <div className={styles.dropdown}>
-                  <AiOutlineMenu size={24} />
-                  <div id="myDropdown" className={styles.dropdownContent}>
-                    <a href="/">Encerrar</a>
-                    <a href="/">Deletar</a>
-                  </div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Suporte</td>
-              <td>Maicon</td>
-              <td>Linha de montagem</td>
-              <td>Parado</td>
-              <td>21/06/2021</td>
-              <td>
-                <div className={styles.dropdown}>
-                  <AiOutlineMenu size={24} />
-                  <div id="myDropdown" className={styles.dropdownContent}>
-                    <a href="/">Encerrar</a>
-                    <a href="/">Deletar</a>
-                  </div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Suporte</td>
-              <td>Fátima</td>
-              <td>JD Edwards</td>
-              <td>Morreu</td>
-              <td>21/06/2021</td>
-              <td>
-                <AiOutlineMenu size={24} />
-              </td>
-            </tr>
-          </tbody>
+          {isLoading ? (
+            <div className={styles.isLoading}>
+              <Loader
+                type="Puff"
+                color="#00BFFF"
+                height={100}
+                width={100}
+                timeout={3000} //3 secs
+              />
+            </div>
+          ) : (
+            <tbody>
+              {tickets.map((ticket: any) => (
+                <tr key={ticket.id}>
+                  <td>{ticket.id}</td>
+                  <td>{ticket.mecanismo}</td>
+                  <td>{ticket.solicitante}</td>
+                  <td>{ticket.categoria}</td>
+                  <td>{ticket.subcategoria}</td>
+                  <td>
+                    {new Date(ticket.data_abertura).toLocaleDateString(
+                      'pt-BR',
+                      {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: 'numeric',
+                      }
+                    )}
+                  </td>
+                  <td>
+                    <div className={styles.dropdown}>
+                      <AiOutlineMenu size={24} />
+                      <div id="myDropdown" className={styles.dropdownContent}>
+                        <a href="/" onClick={() => handleDelete(ticket.id)}>
+                          Deletar
+                        </a>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
         </table>
       </div>
     </main>
